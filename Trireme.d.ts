ebd512dd2b14ffd23186e43e87750a56a428916e
Dom Hastings <dom111@users.noreply.github.com>
2021-01-17 21:09:59 +0000

    Initial commit

---
+++ Trireme.d.ts
@@ -0,0 +1,5 @@
+import NavalTransport from '@civ-clone/base-unit-type-naval-transport/NavalTransport';
+export declare class Trireme extends NavalTransport {
+  capacity(): number;
+}
+export default Trireme;
---
+++ Trireme.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Trireme = void 0;
+const NavalTransport_1 = require("@civ-clone/base-unit-type-naval-transport/NavalTransport");
+class Trireme extends NavalTransport_1.default {
+    capacity() {
+        return 2;
+    }
+}
+exports.Trireme = Trireme;
+exports.default = Trireme;
+//# sourceMappingURL=Trireme.js.map
